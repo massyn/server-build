be84fa868695553377ece9ac9d20caa2755d13e8
Phil Massyn <massyn@users.noreply.github.com>
2017-06-03 14:58:33 +1000

    Create build-it.pl

---
+++ build-it.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+# == the main build file
+
+use strict;
+
+do "library.pl";
+
+# == check if we are running with a sudo'ed root
+&check_sudo();
+
+# == before we do anything else, let's check the Ubuntu version
+my $VER = &ubuntu_version();
+
+if($VER eq 'unknown')
+{
+  die "Sorry, but this version of Ubuntu is not supported.";
+}
+
+my $CONFIG = "/etc/server_build.cfg";
+
+&manager_config($CONFIG);
