c56a991f813e958f1c399850999a60df563d1ec1
Phil Massyn <massyn@users.noreply.github.com>
2017-06-03 21:30:34 +1000

    Update harden.pl

--- harden.pl
+++ harden.pl
@@ -16,24 +16,28 @@ if($VER eq 'unknown')
   die "Sorry, but this version of Ubuntu is not supported.";
 }
 
+&setup_sshd();
+
 sub setup_sshd
 {
         if(!-f "/etc/ssh/sshd_config")
         {
-                die "sshd_config not found!";
+                &log("sshd_config not found!");
         }
-        
-        &param("/etc/ssh/sshd_config","PermitRootLogin","no");
-        &param("/etc/ssh/sshd_config","X11Forwarding","no");
-        &param("/etc/ssh/sshd_config","ChallengeResponseAuthentication","yes");
-        #&param("/etc/ssh/sshd_config","ClientAliveInterval","300");
-        #&param("/etc/ssh/sshd_config","ClientAliveCountMax","0");
-        &param("/etc/ssh/sshd_config","IgnoreRhosts","yes");
-        &param("/etc/ssh/sshd_config","HostbasedAuthentication","no");
-        my $port = &ask("Enter a different SSH port name",1022,1022);
-        &param("/etc/ssh/sshd_config","Port",$port);
-        &param("/etc/ssh/sshd_config","PermitEmptyPasswords","no");
-        &param("/etc/ssh/sshd_config","Banner","/etc/issue");
-        &param("/etc/ssh/sshd_config","AllowTcpForwarding","no");
-        &param("/etc/ssh/sshd_config","LoginGraceTime","30s");
+        else
+        {
+          &param("/etc/ssh/sshd_config","PermitRootLogin","no");
+          &param("/etc/ssh/sshd_config","X11Forwarding","no");
+          &param("/etc/ssh/sshd_config","ChallengeResponseAuthentication","yes");
+          #&param("/etc/ssh/sshd_config","ClientAliveInterval","300");
+          #&param("/etc/ssh/sshd_config","ClientAliveCountMax","0");
+          &param("/etc/ssh/sshd_config","IgnoreRhosts","yes");
+          &param("/etc/ssh/sshd_config","HostbasedAuthentication","no");
+          my $port = &ask("Enter a different SSH port name",1022,1022);
+          &param("/etc/ssh/sshd_config","Port",$port);
+          &param("/etc/ssh/sshd_config","PermitEmptyPasswords","no");
+          &param("/etc/ssh/sshd_config","Banner","/etc/issue");
+          &param("/etc/ssh/sshd_config","AllowTcpForwarding","no");
+          &param("/etc/ssh/sshd_config","LoginGraceTime","30s");
+        }
 }
