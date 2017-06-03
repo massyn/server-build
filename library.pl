9b9cfccf5ec7d26d74969365a79e5b838a8e5a10
Phil Massyn <massyn@users.noreply.github.com>
2017-06-03 14:51:54 +1000

    Update library.pl

--- library.pl
+++ library.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+r#!/usr/bin/perl
 use strict;
 
 # Library file used across the set of server build scripts
@@ -28,7 +28,7 @@ sub ubuntu_version
 {
   # == return the current ubuntu version (which is supported by the script
   
-  my $out = `lsb_release -a 2>&1`;
+  my $out = `lsb_release -r 2>&1`;
   
   if($out =! /Ubuntu 17\.04/)
   {
@@ -39,3 +39,45 @@ sub ubuntu_version
     return "unknown";
   }  
 }
+
+sub manage_config
+{
+  ($CONFIG_FILE) = @_;
+	my %Q;
+
+	# -- if there is a config file, read it
+	if(-f $CONFIG_FILE)
+	{
+		open(IN,"$CONFIG_FILE");
+		foreach my $l (<IN>)
+		{
+			chomp($l);
+			if($l eq '' || $l =~ /^#/)
+			{
+				next;
+			}
+			my ($a,$b) = split(/\=/,$l,2);
+			$Q{$a} = $b;
+		}
+		close IN;
+	}
+
+	$Q{HOSTNAME} = &ask("Enter the server name",`hostname`,$Q{HOSTNAME});
+	$Q{DOMAIN}  = &ask("Enter your domain name",`domainname`,$Q{DOMAIN});
+	$Q{ADMIN} = &ask("Enter the admin email address",'',$Q{ADMIN});
+	$Q{WWWGROUP} = &ask("Enter the name of the web group","webmasters",$Q{WWWGROUP});
+	$Q{ROLEWEB} = &ask("Will this be a web server?","Y",$Q{ROLEWEB});
+	$Q{ROLEDB} = &ask("Will this be a database?","Y",$Q{ROLEDB});
+
+	# -- write the config file to the disk
+	open(OUT,">$CONFIG_FILE") || die "Can't write config file - $!";
+	print OUT "# Config created on " . scalar gmtime(time) . "\n";
+	foreach my $c (sort keys %Q)
+	{
+		print OUT "$c=$Q{$c}\n";
+	}
+	close OUT;
+
+	return %Q;
+}
+
