82c8232072ac092fb1caf08b433300ce118f3669
Phil Massyn <massyn@users.noreply.github.com>
2017-06-04 11:58:12 +1000

    Update configure_virtualhost.pl

--- configure_virtualhost.pl
+++ configure_virtualhost.pl
@@ -1,81 +1,38 @@
 #!/usr/bin/perl
 
-# == the main build file
-
-
+# == Configure additional virtual hosts for Apache
 
 use strict;
-
-
-
 do './library.pl';
-
 &log("Starting $0");
 
-
-
 # == check if we are running with a sudo'ed root
-
 &check_sudo();
-
-
-
 # == before we do anything else, let's check the Ubuntu version
-
 my $VER = &ubuntu_version();
-
-
-
 if($VER eq 'unknown')
-
 {
-
-  die "Sorry, but this version of Ubuntu is not supported.";
-
+	die "Sorry, but this version of Ubuntu is not supported.";
 }
-
-
 
 my $CONFIG = "/etc/server_build.cfg";
 
-
-
 &manage_config($CONFIG);
 
-
-
-
-
 # == read the config
-
 my %Q;
-
 open(IN,"$CONFIG");
-
 foreach my $l (<IN>)
-
 {
-
   chomp($l);
-
-
-
 if($l eq '' || $l =~ /^#/)
-
   {
-
     next;
-
   }
-
   my ($a,$b) = split(/\=/,$l,2);
-
   $Q{$a} = $b;
-
   &log("config : $a = $b");
-
 }
-
 close IN;
 
-&www_virtualhost();
+&www_virtualhost($Q{WWWROOT});
