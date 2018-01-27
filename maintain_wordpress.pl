54a08eab53dc9743646d71b97947efeb8c48de89
Phil Massyn <massyn@users.noreply.github.com>
2018-01-28 08:17:34 +0900

    Create maintain_wordpress.pl

---
+++ maintain_wordpress.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+# == Configure additional virtual hosts for Apache
+
+use strict;
+do './library.pl';
+&log("Starting $0");
+
+# == check if we are running with a sudo'ed root
+&check_sudo();
+# == before we do anything else, let's check the Ubuntu version
+my $VER = &ubuntu_version();
+if($VER eq 'unknown')
+{
+	die "Sorry, but this version of Ubuntu is not supported.";
+}
+
+# == manage the config
+my $CONFIG = "/etc/server_build.cfg";
+my %Q = &manage_config($CONFIG);
+
+if($Q{ROLEWEB} =~ /y/i)
+{
+  # do we have wp-cli installed?  If not, do it first
+  if(!-f '/usr/local/bin/wp')
+  {
+    system('curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o /usr/local/bin/wp');
+    system('chmod +x /usr/local/bin/wp');
+  }
+  
+  # find all sites that could host wordpress
+  opendir(DIR,"$CONFIG{WWWROOT}");
+  foreach my $d (readdir(DIR))
+  {
+    my $dir = "$CONFIG{WWWROOT}/$d";
+    print "$dir\n";
+    if(-f "$dir/wp-config.php")
+    {
+      print " ==> Found Wordpress\n";
+    }
+  }
+}
