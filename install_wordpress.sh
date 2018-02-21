fbbbc8455571b922ab5a0374d413ef438bedfe3e
Phil Massyn <massyn@users.noreply.github.com>
2018-02-22 06:38:16 +1100

    Update install_wordpress.sh

--- install_wordpress.sh
+++ install_wordpress.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+  echo 'You must provide the name of the website domain where to install Wordpress'
+fi
+
+. /etc/server_build.cfg
 curl https://wordpress.org/latest.tar.gz | tar -zxvC /tmp
 
-mv /tmp/wordpress/* /wwwroot/$1/www/
+mv /tmp/wordpress/* $WWWROOT/$1/www/
