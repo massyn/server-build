54ac835fa0ba9537a2d194a9594ce7ad784a7994
Phil Massyn <massyn@users.noreply.github.com>
2018-02-22 06:40:51 +1100

    Update install_wordpress.sh

--- install_wordpress.sh
+++ install_wordpress.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+. /etc/server_build.cfg
+
 if [ -z "$1" ]; then
   echo 'You must provide the name of the website domain where to install Wordpress'
+  exit 1
 fi
 
-. /etc/server_build.cfg
+if [ !-d "$WWWROOT/$1/www"]; then
+  echo 'The directory does not exist.  Maybe you need to create the website first with ./configure_virtualhost.pl'
+  exit 1
+fi
+
 curl https://wordpress.org/latest.tar.gz | tar -zxvC /tmp
 
 mv /tmp/wordpress/* $WWWROOT/$1/www/
