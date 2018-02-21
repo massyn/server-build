48a98767629ea4af188805c6a2fd206ce6040b6e
Phil Massyn <massyn@users.noreply.github.com>
2018-02-22 04:44:36 +1100

    Create buildit-crontab.sh

---
+++ buildit-crontab.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function addtocrontab () {
+        echo "Adding crontab - $2"
+        local frequency=$1
+        local command=$2
+        local job="$frequency $command"
+        cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
+}
+
+addtocrontab "0 0 * * *" "$HOME/server-build/maintain_backup.pl"
