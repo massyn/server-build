d8d9d2b9df6a7125cb3614219f5b772d14a54877
Phil Massyn <massyn@users.noreply.github.com>
2018-02-21 21:49:01 +1100

    Update maintain_backup.pl

--- maintain_backup.pl
+++ maintain_backup.pl
@@ -4,12 +4,31 @@ use strict;
 do './library.pl';
 &log("Starting $0");
 
-# == find all the databases
+# == manage the config
+my $CONFIG = "/etc/server_build.cfg";
+my %Q = &manage_config($CONFIG);
 
+&log("Saving backups to $Q{BACKUP}");
+
+if(!-d $Q{BACKUP})
+{
+        &log("Creating the backup directory");
+        mkdir($Q{BACKUP});
+}
+
+# == backup databases
 foreach my $db (`echo show databases |mysql | grep -v Database | grep -v performance_schema | grep -v information_schema`)
 {
         chomp($db);
-        print "==> $db\n";
+        &log("Backing up database ==> $db");
 
-        system("mysqldump $db > $db.sql");
+        system("mysqldump $db > $Q{BACKUP}/db_$db.sql");
+}
+
+# == backup websites
+foreach my $www (`ls $Q{WWWROOT}`)
+{
+        chomp($www);
+        &log("Backing up website ==> $www");
+        system("tar -czvf $Q{BACKUP}/www_$www.tar $Q{WWWROOT}/$www");
 }
