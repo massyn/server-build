d7b21dcdb85ebc911c25aeffcfe5a039da708d59
Phil Massyn <massyn@users.noreply.github.com>
2018-02-22 14:46:26 +1100

    Update build-crontab.sh

--- build-crontab.sh
+++ build-crontab.sh
@@ -15,7 +15,7 @@ function addtocrontabroot () {
         cat <(fgrep -i -v "$command" <(sudo crontab -l)) <(echo "$job") | sudo crontab -
 }
 
-addtocrontab "0 0 * * *" "cd $HOME/server-build && /usr/bin/perl ./maintain_backup.pl > /tmp/maintain_backup.log 2>&1""
+addtocrontab "0 0 * * *" "cd $HOME/server-build && /usr/bin/perl ./maintain_backup.pl > /tmp/maintain_backup.log 2>&1"
 addtocrontab "* 1 * * *" "cd $HOME/server-build && /usr/bin/perl ./maintain_wordpress.pl > /tmp/maintain_wordpress.log 2>&1"
 addtocrontabroot "* 2 * * 0" "cd $HOME/server-build && ./maintain_letsencrypt.sh > /tmp/maintain_letsencrypt.log >2&1"
 addtocrontabroot "* 2 * * 6" "cd $HOME/server-build && ./maintain_os.sh > /tmp/maintain_os.log >2&1"
