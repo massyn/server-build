34b3956c326c147720deda672f896bbe0f60e90c
Phil Massyn <massyn@users.noreply.github.com>
2018-06-16 15:14:59 +0900

    Create firewall.sh

---
+++ firewall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# A very basic firewall that blocks all incoming ports, except the ssh and web ports (80 & 443)
+
+# == it has to run as root
+
+if [ `whoami` == "root" ]; then
+
+        # == get the sshd port
+        export P=`cat /etc/ssh/sshd_config | grep ^Port | head -1 | awk {'print \$2'}`
+
+
+        # == flush it all
+        iptables -P INPUT ACCEPT
+        iptables -P OUTPUT ACCEPT
+        iptables -P FORWARD ACCEPT
+        iptables -F
+
+        # == setup the rules
+
+        iptables -A INPUT -p tcp --dport 1022 -j ACCEPT
+        iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+        iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+        iptables -A INPUT -j DROP
+
+        iptables -L
+else
+        echo "You are not root.  Run the script as sudo"
+fi
